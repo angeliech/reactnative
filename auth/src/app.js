47d0d9b91fde7be8957dbfb1f15c2f4670d86cb5
Angelie Macalansag <angelie.macalansag@gruden.com>
2017-02-24 15:59:38 +1100

    Auth

--- auth/src/app.js
+++ auth/src/app.js
@@ -1,28 +1,80 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import firebase from 'firebase';
-import { Header } from './components/common';
+import { Header, Button, Spinner, Card, CardSection } from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  state = { loggedIn: null };
+
   componentWillMount() {
-  firebase.initializeApp({
-      apiKey: 'AIzaSyB6bvYxQbuaRV8n-Pty_-ckAnhxa3pmHU0',
-      authDomain: 'authentication-382fc.firebaseapp.com',
-      databaseURL: 'https://authentication-382fc.firebaseio.com',
-      storageBucket: 'authentication-382fc.appspot.com',
-      messagingSenderId: '1070570833532'
+    firebase.initializeApp({
+        apiKey: 'AIzaSyB6bvYxQbuaRV8n-Pty_-ckAnhxa3pmHU0',
+        authDomain: 'authentication-382fc.firebaseapp.com',
+        databaseURL: 'https://authentication-382fc.firebaseio.com',
+        storageBucket: 'authentication-382fc.appspot.com',
+        messagingSenderId: '1070570833532'
     });
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
+  }
+
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return (
+          <Card>
+            <CardSection>
+              <Button onPress={() => firebase.auth().signOut()}>
+                Log Out
+              </Button>
+            </CardSection>
+          </Card>
+        );
+      case false:
+        return <LoginForm />;
+      default:
+        return (
+          <View style={spinnerContainerStyle}>
+            <Spinner size="large" />
+          </View>
+        );
+    }
   }
 
   render() {
     return (
-      <View>
+      <View style={appContainerStyle}>
         <Header headerText="Authentication" />
-        <LoginForm />
+        {this.renderContent()}
       </View>
     );
   }
 }
+
+const styles = {
+  appContainerStyle: {
+    flex: 1
+  },
+  spinnerContainerStyle: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  contentStyle: {
+    justifyContent: 'center',
+    marginTop: 20,
+    alignSelf: 'flex-start'
+
+  }
+};
+
+const { appContainerStyle, spinnerContainerStyle, contentStyle } = styles;
 
 export default App;
